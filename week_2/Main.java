ac8beccc02ee505abab3383a02a8a809de83c0c6
Wai Phone Thaw <waishwe987@gmail.com>
2026-08-04 11:30:27 +0800

    Create Main.java

---
+++ week_2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Student s1 = new Student(101, "Alice", 20, 3.85);
+
+      
+        s1.displayInfo();
+        s1.study();
+        s1.exam();
+    }
+}
